0360f51b5a73752c2419261cfb6fffec269c1c63
Kid Omar Costelo <kidomar.costelo@cit.edu>
2023-02-12 16:13:17 +0800

    Implement hopfield

--- HopfieldNetwork/HopfieldNetwork/Form1.Designer.cs
+++ HopfieldNetwork/HopfieldNetwork/Form1.Designer.cs
@@ -46,6 +46,11 @@
             this.pictureBox13 = new System.Windows.Forms.PictureBox();
             this.pictureBox12 = new System.Windows.Forms.PictureBox();
             this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -70,9 +75,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(27, 23);
+            this.pictureBox1.Location = new System.Drawing.Point(24, 17);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox1.Size = new System.Drawing.Size(83, 72);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -81,9 +87,10 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.White;
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(136, 23);
+            this.pictureBox2.Location = new System.Drawing.Point(119, 17);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox2.Size = new System.Drawing.Size(83, 72);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -92,9 +99,10 @@
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.White;
             this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox3.Location = new System.Drawing.Point(246, 23);
+            this.pictureBox3.Location = new System.Drawing.Point(215, 17);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox3.Size = new System.Drawing.Size(83, 72);
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
@@ -103,9 +111,10 @@
             // 
             this.pictureBox6.BackColor = System.Drawing.Color.White;
             this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox6.Location = new System.Drawing.Point(246, 136);
+            this.pictureBox6.Location = new System.Drawing.Point(215, 102);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox6.Size = new System.Drawing.Size(83, 72);
             this.pictureBox6.TabIndex = 5;
             this.pictureBox6.TabStop = false;
             this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
@@ -114,9 +123,10 @@
             // 
             this.pictureBox5.BackColor = System.Drawing.Color.White;
             this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox5.Location = new System.Drawing.Point(136, 136);
+            this.pictureBox5.Location = new System.Drawing.Point(119, 102);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox5.Size = new System.Drawing.Size(83, 72);
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
             this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
@@ -125,9 +135,10 @@
             // 
             this.pictureBox4.BackColor = System.Drawing.Color.White;
             this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox4.Location = new System.Drawing.Point(27, 136);
+            this.pictureBox4.Location = new System.Drawing.Point(24, 102);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox4.Size = new System.Drawing.Size(83, 72);
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
             this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
@@ -136,9 +147,10 @@
             // 
             this.pictureBox9.BackColor = System.Drawing.Color.White;
             this.pictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox9.Location = new System.Drawing.Point(246, 247);
+            this.pictureBox9.Location = new System.Drawing.Point(215, 185);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox9.Size = new System.Drawing.Size(83, 72);
             this.pictureBox9.TabIndex = 8;
             this.pictureBox9.TabStop = false;
             this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
@@ -147,9 +159,10 @@
             // 
             this.pictureBox8.BackColor = System.Drawing.Color.White;
             this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox8.Location = new System.Drawing.Point(136, 247);
+            this.pictureBox8.Location = new System.Drawing.Point(119, 185);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox8.Size = new System.Drawing.Size(83, 72);
             this.pictureBox8.TabIndex = 7;
             this.pictureBox8.TabStop = false;
             this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
@@ -158,9 +171,10 @@
             // 
             this.pictureBox7.BackColor = System.Drawing.Color.White;
             this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox7.Location = new System.Drawing.Point(27, 247);
+            this.pictureBox7.Location = new System.Drawing.Point(24, 185);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox7.Size = new System.Drawing.Size(83, 72);
             this.pictureBox7.TabIndex = 6;
             this.pictureBox7.TabStop = false;
             this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
@@ -169,107 +183,156 @@
             // 
             this.pictureBox19.BackColor = System.Drawing.Color.White;
             this.pictureBox19.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox19.Location = new System.Drawing.Point(648, 247);
+            this.pictureBox19.Location = new System.Drawing.Point(521, 185);
+            this.pictureBox19.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox19.Name = "pictureBox19";
-            this.pictureBox19.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox19.Size = new System.Drawing.Size(83, 72);
             this.pictureBox19.TabIndex = 17;
             this.pictureBox19.TabStop = false;
-            this.pictureBox19.Click += new System.EventHandler(this.pictureBox19_Click);
             // 
             // pictureBox18
             // 
             this.pictureBox18.BackColor = System.Drawing.Color.White;
             this.pictureBox18.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox18.Location = new System.Drawing.Point(535, 247);
+            this.pictureBox18.Location = new System.Drawing.Point(422, 185);
+            this.pictureBox18.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox18.Name = "pictureBox18";
-            this.pictureBox18.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox18.Size = new System.Drawing.Size(83, 72);
             this.pictureBox18.TabIndex = 16;
             this.pictureBox18.TabStop = false;
-            this.pictureBox18.Click += new System.EventHandler(this.pictureBox18_Click);
             // 
             // pictureBox17
             // 
             this.pictureBox17.BackColor = System.Drawing.Color.White;
             this.pictureBox17.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox17.Location = new System.Drawing.Point(424, 247);
+            this.pictureBox17.Location = new System.Drawing.Point(325, 185);
+            this.pictureBox17.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox17.Name = "pictureBox17";
-            this.pictureBox17.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox17.Size = new System.Drawing.Size(83, 72);
             this.pictureBox17.TabIndex = 15;
             this.pictureBox17.TabStop = false;
-            this.pictureBox17.Click += new System.EventHandler(this.pictureBox17_Click);
             // 
             // pictureBox16
             // 
             this.pictureBox16.BackColor = System.Drawing.Color.White;
             this.pictureBox16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox16.Location = new System.Drawing.Point(648, 136);
+            this.pictureBox16.Location = new System.Drawing.Point(521, 102);
+            this.pictureBox16.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox16.Name = "pictureBox16";
-            this.pictureBox16.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox16.Size = new System.Drawing.Size(83, 72);
             this.pictureBox16.TabIndex = 14;
             this.pictureBox16.TabStop = false;
-            this.pictureBox16.Click += new System.EventHandler(this.pictureBox16_Click);
             // 
             // pictureBox15
             // 
             this.pictureBox15.BackColor = System.Drawing.Color.White;
             this.pictureBox15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox15.Location = new System.Drawing.Point(535, 136);
+            this.pictureBox15.Location = new System.Drawing.Point(422, 102);
+            this.pictureBox15.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox15.Name = "pictureBox15";
-            this.pictureBox15.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox15.Size = new System.Drawing.Size(83, 72);
             this.pictureBox15.TabIndex = 13;
             this.pictureBox15.TabStop = false;
-            this.pictureBox15.Click += new System.EventHandler(this.pictureBox15_Click);
             // 
             // pictureBox14
             // 
             this.pictureBox14.BackColor = System.Drawing.Color.White;
             this.pictureBox14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox14.Location = new System.Drawing.Point(424, 136);
+            this.pictureBox14.Location = new System.Drawing.Point(325, 102);
+            this.pictureBox14.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox14.Name = "pictureBox14";
-            this.pictureBox14.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox14.Size = new System.Drawing.Size(83, 72);
             this.pictureBox14.TabIndex = 12;
             this.pictureBox14.TabStop = false;
-            this.pictureBox14.Click += new System.EventHandler(this.pictureBox14_Click);
             // 
             // pictureBox13
             // 
             this.pictureBox13.BackColor = System.Drawing.Color.White;
             this.pictureBox13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox13.Location = new System.Drawing.Point(648, 23);
+            this.pictureBox13.Location = new System.Drawing.Point(521, 17);
+            this.pictureBox13.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox13.Name = "pictureBox13";
-            this.pictureBox13.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox13.Size = new System.Drawing.Size(83, 72);
             this.pictureBox13.TabIndex = 11;
             this.pictureBox13.TabStop = false;
-            this.pictureBox13.Click += new System.EventHandler(this.pictureBox13_Click);
             // 
             // pictureBox12
             // 
             this.pictureBox12.BackColor = System.Drawing.Color.White;
             this.pictureBox12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox12.Location = new System.Drawing.Point(535, 23);
+            this.pictureBox12.Location = new System.Drawing.Point(422, 17);
+            this.pictureBox12.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox12.Name = "pictureBox12";
-            this.pictureBox12.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox12.Size = new System.Drawing.Size(83, 72);
             this.pictureBox12.TabIndex = 10;
             this.pictureBox12.TabStop = false;
-            this.pictureBox12.Click += new System.EventHandler(this.pictureBox12_Click);
             // 
             // pictureBox11
             // 
             this.pictureBox11.BackColor = System.Drawing.Color.White;
             this.pictureBox11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox11.Location = new System.Drawing.Point(424, 23);
+            this.pictureBox11.Location = new System.Drawing.Point(325, 17);
+            this.pictureBox11.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(95, 95);
+            this.pictureBox11.Size = new System.Drawing.Size(83, 72);
             this.pictureBox11.TabIndex = 9;
             this.pictureBox11.TabStop = false;
-            this.pictureBox11.Click += new System.EventHandler(this.pictureBox11_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Learn";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "input matrix";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "output matrix";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(215, 278);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 23);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(215, 308);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(365, 23);
+            this.textBox2.TabIndex = 21;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(857, 414);
+            this.ClientSize = new System.Drawing.Size(636, 384);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox19);
             this.Controls.Add(this.pictureBox18);
             this.Controls.Add(this.pictureBox17);
@@ -288,6 +351,7 @@
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -309,6 +373,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -332,5 +397,10 @@
         private PictureBox pictureBox13;
         private PictureBox pictureBox12;
         private PictureBox pictureBox11;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- HopfieldNetwork/HopfieldNetwork/Form1.cs
+++ HopfieldNetwork/HopfieldNetwork/Form1.cs
@@ -1,117 +1,122 @@
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Single;
+using System.Formats.Asn1;
+
 namespace HopfieldNetwork
 {
     public partial class Form1 : Form
     {
+
+        private Matrix<float> inputMatrix;
+        private Matrix<float> outputMatrix;
+
+        private Matrix<float> weight = Matrix<float>.Build.DenseOfArray(new float[,]
+            {
+                { 0, 0, 2, -2, -2, -2, 2, 0, 2 },
+                { 0, 0, 0, 0, 0, 0, 0, 2, 0 },
+                { 2, 0, 0, -2, -2, -2, 2, 0, 2 },
+                { 2, 0, -2, 0, 2, 2, -2, 0 ,-2 },
+                { 2, 0, -2, 2, 0, 2, -2, 0, -2 },
+                { 2, 0, -2, 2, 2, 0, -2, 0, -2 },
+                { 2, 0, 2, -2, -2, -2, 0, 0, -2},
+                { 0, 2, 0, 0, 0, 0, 0, 0, 0},
+                { 2, 0, 2, -2, -2, -2, 2, 0, 0 }
+            });
+
+        private int Rules(float num)
+        {
+            if (num > 0) return 1;
+            return -1;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            inputMatrix = Matrix<float>.Build.Dense(9, 1, -1);
+            outputMatrix = Matrix<float>.Build.Dense(9, 1, -1);
         }
-
-        public void boxClick(PictureBox pb) 
+        public int boxClick(PictureBox pb) 
         {
             if(pb.BackColor == Color.Black)
             {
                 pb.BackColor = Color.White;
+                return -1;
             }
             else
             {
                 pb.BackColor = Color.Black;
+                return 1;
             }
+        }
+
+        public Color boxAssign(float num)
+        {
+            if ((int)num >= 1)
+            {
+                return Color.Black;
+            }
+            return Color.White;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox1);
+            inputMatrix[0,0] = boxClick(pictureBox1);
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox2);
+            inputMatrix[1, 0] = boxClick(pictureBox2);
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox3);
+            inputMatrix[2, 0] =  boxClick(pictureBox3);
         }
 
         private void pictureBox4_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox4);
+            inputMatrix[3, 0] = boxClick(pictureBox4);
         }
 
         private void pictureBox5_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox5);
+            inputMatrix[4, 0] = boxClick(pictureBox5);
         }
 
         private void pictureBox6_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox6);
+            inputMatrix[5, 0] = boxClick(pictureBox6);
         }
 
         private void pictureBox7_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox7);
+            inputMatrix[6, 0] = boxClick(pictureBox7);
         }
 
         private void pictureBox8_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox8);
+            inputMatrix[7, 0] = boxClick(pictureBox8);
         }
 
         private void pictureBox9_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox9);
+            inputMatrix[8, 0] = boxClick(pictureBox9);
         }
 
-        private void pictureBox11_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            boxClick(pictureBox11);
-        }
-
-        private void pictureBox12_Click(object sender, EventArgs e)
-        {
-            boxClick(pictureBox12);
-        }
-
-        private void pictureBox13_Click(object sender, EventArgs e)
-        {
-            boxClick(pictureBox13);
-        }
-
-        private void pictureBox14_Click(object sender, EventArgs e)
-        {
-            boxClick(pictureBox14);
-        }
-
-        private void pictureBox15_Click(object sender, EventArgs e)
-        {
-            boxClick(pictureBox15);
-        }
-
-        private void pictureBox16_Click(object sender, EventArgs e)
-        {
-            boxClick(pictureBox16);
-        }
-
-        private void pictureBox17_Click(object sender, EventArgs e)
-        {
-            boxClick(pictureBox17);
-        }
-
-        private void pictureBox18_Click(object sender, EventArgs e)
-        {
-            boxClick(pictureBox18);
-        }
-
-        private void pictureBox19_Click(object sender, EventArgs e)
-        {
-            boxClick(pictureBox19);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
+            outputMatrix = weight.Multiply(inputMatrix);
+            textBox1.Text = outputMatrix.ToString();
+            textBox2.Text = inputMatrix.ToString();
+            pictureBox11.BackColor = boxAssign(outputMatrix[0, 0]);
+            pictureBox12.BackColor = boxAssign(outputMatrix[1, 0]);
+            pictureBox13.BackColor = boxAssign(outputMatrix[2, 0]);
+            pictureBox14.BackColor = boxAssign(outputMatrix[3, 0]);
+            pictureBox15.BackColor = boxAssign(outputMatrix[4, 0]);
+            pictureBox16.BackColor = boxAssign(outputMatrix[5, 0]);
+            pictureBox17.BackColor = boxAssign(outputMatrix[6, 0]);
+            pictureBox18.BackColor = boxAssign(outputMatrix[7, 0]);
+            pictureBox19.BackColor = boxAssign(outputMatrix[8, 0]);
         }
     }
 }
